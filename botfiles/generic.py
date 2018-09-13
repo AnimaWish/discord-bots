1fd8471e7cdf71798d309aba7721c6e676452e26
Monolith\Caius <animawish@gmail.com>
2018-09-12 23:59:32 -0600

    bad comment

--- botfiles/generic.py
+++ botfiles/generic.py
@@ -378,7 +378,7 @@ class DiscordBot:
 
     async def logToChannel(self, message):
         #await self.client.send_message(self.client.get_server(DiscordBot.LOGS_SERVER_ID).get_channel(DiscordBot.LOGS_CHANNEL_ID), message)
-        #print("{}: {}".format(self.client.user.name, message))
+        print("{}: {}".format(self.client.user.name, message))
 
     async def on_ready(self):
         print('Logged in as {} ({})'.format(self.client.user.name, self.client.user.id))
