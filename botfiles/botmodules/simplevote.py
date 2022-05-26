1244f750598b32170e78cb38bd9ec797762db2e3
Monolith\Caius <animawish@gmail.com>
2022-05-26 17:34:26 -0600

    fix rankvote cutoff bug

--- botfiles/botmodules/simplevote.py
+++ botfiles/botmodules/simplevote.py
@@ -94,13 +94,17 @@ class SimpleVoteBot(DiscordBot):
         text = self.constructBallotMessage(referendum, choices, emoji, "*Click emoji in order from favorite to least favorite! Remember to double check your ballot!*")
         choiceMessage = await message.channel.send(text)
 
-        text2 = self.constructRankChoiceResultsMessage(referendum, choices, emoji, [], {}, {})
-        resultMessage = await message.channel.send(text2)
+        text2 = self.constructRankChoiceResultBallotsMessage(referendum, choices, emoji, [], {}, {})
+        resultBallotsMessage = await message.channel.send(text2)
+
+        text3 = self.constructRankChoiceResultStandingsMessage(referendum, choices, emoji, [], {}, {})
+        resultStandingsMessage = await message.channel.send(text3)
 
         self.elections[choiceMessage.id] = {
             "ownerId": message.author.id,
             "ballotMessage": choiceMessage,
-            "resultMessage": resultMessage,
+            "resultBallotsMessage": resultBallotsMessage,
+            "resultStandingsMessage": resultStandingsMessage,
             "text": referendum,
             "choices": choices,
             "emoji": emoji,
@@ -151,6 +155,9 @@ class SimpleVoteBot(DiscordBot):
         def sequence_gamix(n,i):
             return min(sequence_geometric(n,i,2),sequence_arithmetic(n,i,1))
 
+        def sequence_gamix_frac(n,i,c):
+            return c * (float(sequence_gamix(n,i)) / n)
+
         strengths = [int(sequence_gamix(len(electionObj["choices"]),i)) for i in range(len(electionObj["choices"]))]
         standings = [0]*len(electionObj["choices"])
         for ballot in electionObj["ballots"]:
@@ -160,8 +167,10 @@ class SimpleVoteBot(DiscordBot):
         if isClosed:
             await electionObj["ballotMessage"].edit(content=self.constructBallotMessage(electionObj["text"], electionObj["choices"], electionObj["emoji"], "*This poll is now closed!*"))
 
-        outputString = self.constructRankChoiceResultsMessage(electionObj["text"], electionObj["choices"], electionObj["emoji"], standings, electionObj["ballots"], electionObj["names"], isClosed)
-        await electionObj["resultMessage"].edit(content=outputString)
+        ballotsOutputString = self.constructRankChoiceResultsMessage(electionObj["text"], electionObj["choices"], electionObj["emoji"], standings, electionObj["ballots"], electionObj["names"], isClosed)
+        standingsOutputString = self.constructRankChoiceResultsMessage(electionObj["text"], electionObj["choices"], electionObj["emoji"], standings, electionObj["ballots"], electionObj["names"], isClosed)
+        await electionObj["resultBallotsMessage"].edit(content=ballotsOutputString)
+        await electionObj["resultStandingsMessage"].edit(content=standingsOutputString)
         if isClosed:
             del self.elections[reactionPayload.message_id] 
     
@@ -174,7 +183,7 @@ class SimpleVoteBot(DiscordBot):
         output += "\n" + helpString
 
         return output
-        
+
     #
     # referendum: string          referendum
     # choices: []string           choices, ordered
@@ -184,7 +193,7 @@ class SimpleVoteBot(DiscordBot):
     # names: {userId: []string}   usernames, per user
     # isClosed: bool
     #
-    def constructRankChoiceResultsMessage(self, referendum, choices, emoji, standings, ballots, names, isClosed=False):
+    def constructRankChoiceResultBallotsMessage(self, referendum, choices, emoji, standings, ballots, names, isClosed=False):
         # Determine padding information for Ballots
         longestNameLength = 0
         for name in names.values():
@@ -193,6 +202,32 @@ class SimpleVoteBot(DiscordBot):
         voteListPaddings = {}
         for userId, name in names.items():
             voteListPaddings[userId] = " "*(longestNameLength - len(name))
+
+        output = self.ZERO_WIDTH_SPACE + "\n**__Ballots__**\n"
+        if len(ballots) > 0:
+            for userId, votes in ballots.items():
+                if len(votes) == 0:
+                    continue
+                voteListString = ""
+                for i, vote in enumerate(votes):
+                    voteListString += "**{}.** {} ".format(i + 1, emoji[vote])
+                output += "`{}{}` {}\n".format(names[userId], voteListPaddings[userId], voteListString)
+        else:
+            output += "*No ballots have been cast!*\n"
+
+    #
+    # referendum: string          referendum
+    # choices: []string           choices, ordered
+    # emoji: []emoji              emoji ordered by choices
+    # standings: []int            vote counts, ordered by choices
+    # ballots: {userId: []int}    vote orders, per user
+    # names: {userId: []string}   usernames, per user
+    # isClosed: bool
+    #
+    def constructRankChoiceResultStandingsMessage(self, referendum, choices, emoji, standings, ballots, names, isClosed=False):
+        MAX_EMOJI_PER_MESSAGE = 199
+        MAX_BAR_LENGTH = 12
+        output = self.ZERO_WIDTH_SPACE + "\n**__Standings__**\n"
 
         # Determine padding information for Standings
         longestStandingLength = 0
@@ -210,6 +245,11 @@ class SimpleVoteBot(DiscordBot):
         for i in range(len(standings)):
             standingsPaddings[i] = " "*(longestStandingLength - len(str(standings[i])))
 
+        # Determine how many "elements"/"emoji" from maximum are consumed by static labels
+        maxEmojiBufferCount = 4 # bold for "Standings" + 2 ballot emoji + italics for "Thank you"
+        if isClosed:
+            maxEmojiBufferCount = maxEmojiBufferCount + 2 * len(currentLeaders) # emoji and bold for each leader
+
         # Determine proportion information for standings
         standingsProportions = {}
         for i in range(len(standings)):
@@ -217,28 +257,28 @@ class SimpleVoteBot(DiscordBot):
             if largestStandingValue > 0:
                 standingsProportions[i] = float(standings[i]) / float(largestStandingValue)
 
-        output = self.ZERO_WIDTH_SPACE + "\n**__Ballots__**\n"
-        if len(ballots) > 0:
-            for userId, votes in ballots.items():
-                if len(votes) == 0:
-                    continue
-                voteListString = ""
-                for i, vote in enumerate(votes):
-                    voteListString += "**{}.** {} ".format(i + 1, emoji[vote])
-                output += "`{}{}` {}\n".format(names[userId], voteListPaddings[userId], voteListString)
-        else:
-            output += "*No ballots have been cast!*\n"
-        output += "\n**__Standings__**\n"
         if len(standings) > 0:
+            standingEmojiCounts = []
+            totalEmojiCount = 999999999999999 # sum of standingEmojiCounts
+            currentBarLength = MAX_BAR_LENGTH # decremented per iteration of while loop
+            # Determine the number of emoji per bar, scaling the bar lengths down until we are sure they will all fit in the limit
+            while totalEmojiCount > (MAX_EMOJI_PER_MESSAGE - maxEmojiBufferCount):
+                standingEmojiCounts = []
+                totalEmojiCount = 0
+                for i in range(len(standings)):
+                    numEmoji = 0
+                    if largestStandingValue > 0 and largestStandingValue <= currentBarLength:
+                        numEmoji = standings[i]
+                    elif largestStandingValue > currentBarLength:
+                        numEmoji = int(math.floor(currentBarLength * standingsProportions[i]))
+                    if numEmoji == 0 and standings[i] > 0:
+                        numEmoji = 1
+                    standingEmojiCounts.append(numEmoji)
+                    totalEmojiCount = totalEmojiCount + numEmoji
+                currentBarLength = currentBarLength - 1
+            
             for i in range(len(standings)):
-                numEmoji = 0
-                if largestStandingValue > 0 and largestStandingValue <= 12:
-                    numEmoji = standings[i]
-                elif largestStandingValue > 12:
-                    numEmoji = int(math.floor(12 * standingsProportions[i]))
-                if numEmoji == 0 and standings[i] > 0:
-                    numEmoji = 1
-                output += "{} `{}{}` {}\n".format(emoji[i], standingsPaddings[i], standings[i], (emoji[i]+self.ZERO_WIDTH_SPACE)*numEmoji)
+                output += "{} `{}{}` {}\n".format(emoji[i], standingsPaddings[i], standings[i], (emoji[i]+self.ZERO_WIDTH_SPACE)*standingEmojiCounts[i])
         else:
             output += "*No ballots have been cast!*\n"
         if isClosed:
@@ -280,7 +320,9 @@ class SimpleVoteBot(DiscordBot):
 
         # self.elections[message.id] = {
         #     "ownerId": string,
-        #     "message": messageObj,
+        #     "ballotMessage": message,
+        #     "resultBallotsMessage": message,
+        #     "resultStandingsMessage": message,
         #     "text": string,
         #     "choices": []string,
         #     "emoji": []emoji,
